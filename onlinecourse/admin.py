4d98f21fdac918417cbab5a82d133ce343bd3347
HalluxPollexEarlobe <soroka.victor.my@gmail.com>
2022-08-19 13:53:24 +0100

    Samaah changes implemented

---
+++ onlinecourse/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+# <HINT> Import any new Models here
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+
+# <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 1
+
+# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline, QuestionInline]
+    #inlines = [QuestionInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question']
+    inlines = [ChoiceInline]
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+# <HINT> Register Question and Choice models here
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+
